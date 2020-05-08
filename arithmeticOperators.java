82afe8012d65e692e65148d70d85e8e81105fbeb
Deepakshi0409 <55615304+Deepakshi0409@users.noreply.github.com>
2020-05-08 16:14:46 +0530

    Add files via upload

---
+++ arithmeticOperators.java
@@ -0,0 +1,18 @@
+package demo;
+
+public class arithmeticOperators {
+
+	public static void main(String[] args) {
+		int a,b;
+		a = 20;
+		b = 10;
+		System.out.println("The sum is " + (a+b));
+		System.out.println("The difference is " + (a-b));
+		System.out.println("The product is " + (a*b));
+		System.out.println("The quotient is " + (a/b));
+		System.out.println("The remainder is " + (a%b));
+		
+
+	}
+
+}
