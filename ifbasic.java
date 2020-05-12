dbda2114261a38e0b406f10358c9aa3589740e08
Deepakshi0409 <55615304+Deepakshi0409@users.noreply.github.com>
2020-05-12 17:00:07 +0530

    Add files via upload

---
+++ ifbasic.java
@@ -0,0 +1,17 @@
+package ifBasic;
+
+public class ifbasic {
+
+	public static void main(String[] args) {
+		int a,b;
+		a = 20;
+		b = 10;
+		if(a>b) {
+			System.out.println("a is greater than b");
+		} else {
+			System.out.println("b is grater than a");
+		}
+
+	}
+
+}
