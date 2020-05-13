833da648f8b85a3350cc65b91dd4952d9ba5d260
Deepakshi0409 <55615304+Deepakshi0409@users.noreply.github.com>
2020-05-13 18:37:30 +0530

    Add files via upload

---
+++ basic.java
@@ -0,0 +1,11 @@
+package string;
+
+public class basic {
+
+	public static void main(String[] args) {
+		String text = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		System.out.println("The lenght of the text string is : "+text.length());
+
+	}
+
+}
