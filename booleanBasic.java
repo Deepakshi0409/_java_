0f59ade9b9689314bcc08b5755c4ea3bce9aab1b
Deepakshi0409 <55615304+Deepakshi0409@users.noreply.github.com>
2020-05-14 17:51:48 +0530

    Add files via upload

---
+++ booleanBasic.java
@@ -0,0 +1,13 @@
+package fun;
+
+public class booleanBasic {
+
+	public static void main(String[] args) {
+		boolean isJavaFun=true;
+		boolean isFishTasty=false;
+		System.out.println(isJavaFun);
+		// TODO Auto-generated method stub
+
+	}
+
+}
