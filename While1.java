1c56ffa95a117d5842c418a48a8f79ac5a5948f3
Deepakshi0409 <55615304+Deepakshi0409@users.noreply.github.com>
2020-05-16 17:07:02 +0530

    Add files via upload

---
+++ While1.java
@@ -0,0 +1,15 @@
+package demo;
+
+public class While1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = 0;
+		while (i<5) {
+			System.out.println(i);
+			i++;
+		}
+
+	}
+
+}
