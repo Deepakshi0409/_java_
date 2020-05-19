2635cb294140cc07c90ead40498235b00fd73f9e
Deepakshi0409 <55615304+Deepakshi0409@users.noreply.github.com>
2020-05-19 11:01:53 +0530

    Add files via upload

---
+++ ForLoop1.java
@@ -0,0 +1,15 @@
+package demo;
+
+public class ForLoop1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i = 0; i<11; i++) {
+			if(i==4) {
+				continue;
+			} 
+			System.out.println(i);
+		}
+	}
+
+}
