1332aafdad431f0710ca650ef3465404f03a69f8
Deepakshi0409 <55615304+Deepakshi0409@users.noreply.github.com>
2020-05-21 11:11:05 +0530

    Add files via upload

---
+++ StringCars.java
@@ -0,0 +1,13 @@
+package string;
+
+public class StringCars {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String[] cars = {"Volvo","BMW","Ford","Mazda"};
+		for (String i : cars) {
+			System.out.println(i);
+		}
+	}
+
+}
