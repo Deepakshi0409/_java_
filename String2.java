7b3e53757728f5c2533714ec13037c779d3bdebc
Deepakshi0409 <55615304+Deepakshi0409@users.noreply.github.com>
2020-05-23 12:38:26 +0530

    Add files via upload

---
+++ String2.java
@@ -0,0 +1,11 @@
+package string;
+
+public class String2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String[]cars = {"Volvo", "BMW", "Ford"};
+		System.out.println(cars.length);
+
+}
+}
