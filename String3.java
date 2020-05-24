9c70a5ab37490743b86890bb0ea9fbb84ee19424
Deepakshi0409 <55615304+Deepakshi0409@users.noreply.github.com>
2020-05-24 18:54:53 +0530

    Add files via upload

---
+++ String3.java
@@ -0,0 +1,12 @@
+package string;
+
+public class String3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int[]myNum={10,20,30,40};
+		System.out.println(myNum[0]);
+	}
+
+}
