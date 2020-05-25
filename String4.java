67794ca176ae393857fa1accce12bc7c629b542e
Deepakshi0409 <55615304+Deepakshi0409@users.noreply.github.com>
2020-05-25 11:34:49 +0530

    Add files via upload

---
+++ String4.java
@@ -0,0 +1,13 @@
+package string;
+
+public class String4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String[]cars = {"BMW","Volvo","Ford","Opel"};
+		for (int i =0; i<cars.length;i++) {
+			System.out.println(cars[i]);
+		}
+		}
+
+}
