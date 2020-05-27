c542dc78757ff60de19fe2a7e9ac91353450be6b
Deepakshi0409 <55615304+Deepakshi0409@users.noreply.github.com>
2020-05-27 11:46:41 +0530

    Add files via upload

---
+++ String5.java
@@ -0,0 +1,13 @@
+package string;
+
+public class String5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[][]myNumbers = {{1,2,3,4},{5,6,7}};
+		int x = myNumbers[1][2];
+		System.out.println(x);
+
+	}
+
+}
