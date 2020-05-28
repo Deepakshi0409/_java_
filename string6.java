7c49f45e057d7947bd305f1217f91f3f26017189
Deepakshi0409 <55615304+Deepakshi0409@users.noreply.github.com>
2020-05-28 11:47:16 +0530

    Add files via upload

---
+++ string6.java
@@ -0,0 +1,13 @@
+package string;
+
+public class string6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String[]num= {"1","2","3","4","5","6","7","8","9","10"};
+		for (int i = 0; i<num.length;i++) {
+			System.out.println(i);
+		}
+	}
+
+}
