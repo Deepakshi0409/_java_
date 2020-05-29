bfa81ffbe86a1ca641de0e6e211074bfb7db24f1
Deepakshi0409 <55615304+Deepakshi0409@users.noreply.github.com>
2020-05-29 18:42:41 +0530

    Add files via upload

---
+++ String7.java
@@ -0,0 +1,13 @@
+package string;
+
+public class String7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String firstname = "John";
+		String secondname = " doe";
+		String fullname = firstname +  secondname;
+		System.out.println(fullname);
+	}
+
+}
