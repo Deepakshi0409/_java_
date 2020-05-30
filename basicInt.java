f8819633e0cd6e4940962efaecf1c3f394f3d538
Deepakshi0409 <55615304+Deepakshi0409@users.noreply.github.com>
2020-05-30 11:39:25 +0530

    Add files via upload

---
+++ basicInt.java
@@ -0,0 +1,16 @@
+package myApplication;
+
+public class basicInt {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a,b,c,d,e;
+		a = 10;
+		b = 20;
+		c = 30;
+		d = 40;
+		e = 10;
+		System.out.println(((a+b)*(c+d))/e);
+	}
+
+}
