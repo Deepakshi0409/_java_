e0786d5f942ea5748a976e4be532480e043e9832
Deepakshi0409 <55615304+Deepakshi0409@users.noreply.github.com>
2020-05-31 14:38:37 +0530

    Add files via upload

---
+++ StringTxt.java
@@ -0,0 +1,12 @@
+package string;
+
+public class StringTxt {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String txt = "Please locate where 'locate' occurs !";
+		System.out.println(txt.indexOf("locate"));
+
+	}
+
+}
