0f566b8a945aa39122fd95cccfeb61fe8c80ab65
Deepakshi0409 <55615304+Deepakshi0409@users.noreply.github.com>
2020-06-01 11:30:59 +0530

    Add files via upload

---
+++ StringConcatenate.java
@@ -0,0 +1,12 @@
+package string;
+
+public class StringConcatenate {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String Firstname = "John";
+		String Lastname = " Doe";
+		System.out.println(Firstname.concat(Lastname));
+	}
+
+}
