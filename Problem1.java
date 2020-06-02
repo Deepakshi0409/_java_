6857efa37a419b1f9081d5c6f9394b8700f22431
Deepakshi0409 <55615304+Deepakshi0409@users.noreply.github.com>
2020-06-02 11:36:12 +0530

    Add files via upload

---
+++ Problem1.java
@@ -0,0 +1,15 @@
+package demo;
+
+public class Problem1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a,b,c,d;
+		a = 20;
+		b = 10 + a;
+		c = (a+b) * (a-b);
+		d = c + (a*b);
+		System.out.println(d);
+	}
+
+}
