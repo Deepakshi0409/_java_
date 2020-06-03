d842f07a4e1b2031e016a751605fa51193ab3b0a
Deepakshi0409 <55615304+Deepakshi0409@users.noreply.github.com>
2020-06-03 11:42:49 +0530

    Add files via upload

---
+++ intString.java
@@ -0,0 +1,14 @@
+package string;
+
+public class intString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String x = "10";
+		String y = "20";
+		String Z = x+y;
+		System.out.println(Z);
+				
+	}
+
+}
