0d4977b46845cddf6c8b85e65c84bb297ad76966
Deepakshi0409 <55615304+Deepakshi0409@users.noreply.github.com>
2020-06-05 11:45:17 +0530

    Add files via upload

---
+++ mathMax.java
@@ -0,0 +1,12 @@
+package demo;
+
+public class mathMax {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Math.max(5, 10);
+		System.out.println(Math.max(5, 10));
+
+	}
+
+}
