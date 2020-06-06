bfd96c1641fd40bb87bd521853eb6c26b54cf32a
Deepakshi0409 <55615304+Deepakshi0409@users.noreply.github.com>
2020-06-06 09:48:21 +0530

    Add files via upload

---
+++ SquareRoot.java
@@ -0,0 +1,13 @@
+package demo;
+
+public class SquareRoot {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Math.max(5, 10);
+		Math.sqrt(64);
+		System.out.println((Math.max(5, 10))+(Math.sqrt(64)));
+
+	}
+
+}
